b6aa3997b82c83a6149a0f8b9381b906a909457d
b-neo <80798736+b-neo@users.noreply.github.com>
2021-03-18 14:20:16 +0900

    Update 1.sol

--- 1.sol
+++ 1.sol
@@ -4,7 +4,15 @@ contract Example005 {
   function operation() returns (uint result) {
     result = sum(11,5);
   }
-
+function operation2() returns (uint result) {
+    result = gop(11,5);
+  }
+  function operation3() returns (uint result) {
+    result = nanugi(11,5);
+  }
+  function operation4() returns (uint result) {
+    result = bbagi(11,5);
+  }
   function sum(uint a, uint b) returns (uint) {
       uint result = a + b;
       return result;
